dd6bedce077f778f3d66f0b199bac520524a37a7
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-01-29 00:41:59 +0530

    A pretty ast print example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
+#include <iostream>
+#include <memory>
 #include <popl/driver.hpp>
 
+#include "popl/grammar/ast_printer.hpp"
+#include "popl/grammar/expr.hpp"
+#include "popl/lexer/token.hpp"
+#include "popl/lexer/token_types.hpp"
+#include "popl/literal.hpp"
+
+using namespace popl;
 int main(int argc, char** argv) {
-    popl::Driver driver{};
-    driver.Init(argc, argv);
-};
+    Expr       expression{BinaryExpr{
+        std::make_unique<Expr>(
+            UnaryExpr{Token{TokenType::MINUS, "-", {}, 1},
+                      std::make_unique<Expr>(LiteralExpr{PopLObject{123.0}})}),
+
+        Token{TokenType::STAR, "*", {}, 1},
+
+        std::make_unique<Expr>(GroupingExpr{
+            std::make_unique<Expr>(LiteralExpr{PopLObject{45.67}})})}};
+    AstPrinter printer;
+    std::cout << printer.Print(expression);
+    // popl::Driver driver{};
+    // driver.Init(argc, argv);
+}
