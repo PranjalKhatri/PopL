68eb54489b315c59a0f44a73bd50df6681fa6647
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-01-30 23:41:34 +0530

    reverted to driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,7 @@
-#include <iostream>
-#include <memory>
 #include <popl/driver.hpp>
-
-#include "popl/lexer/token.hpp"
-#include "popl/lexer/token_types.hpp"
-#include "popl/literal.hpp"
-#include "popl/syntax/ast/expr.hpp"
-#include "popl/syntax/visitors/ast_printer.hpp"
 
 using namespace popl;
 int main(int argc, char** argv) {
-    Expr       expression{BinaryExpr{
-        std::make_unique<Expr>(
-            UnaryExpr{Token{TokenType::MINUS, "-", {}, 1},
-                      std::make_unique<Expr>(LiteralExpr{PopLObject{123.0}})}),
-
-        Token{TokenType::STAR, "*", {}, 1},
-
-        std::make_unique<Expr>(GroupingExpr{
-            std::make_unique<Expr>(LiteralExpr{PopLObject{45.67}})})}};
-    AstPrinter printer;
-    std::cout << printer.Print(expression);
-    // popl::Driver driver{};
-    // driver.Init(argc, argv);
+    popl::Driver driver{};
+    driver.Init(argc, argv);
 }
