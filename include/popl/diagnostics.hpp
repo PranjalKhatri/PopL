f3eac6c7def4793f12aee33175f8c68bf02c531d
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-02-01 23:23:57 +0530

    Added runtime error report method and HadRunTimeError method

--- include/popl/diagnostics.hpp
+++ include/popl/diagnostics.hpp
@@ -3,13 +3,17 @@
 #include <string_view>
 
 #include "popl/lexer/token.hpp"
+#include "popl/syntax/Exceptions/run_time_error.hpp"
 
 namespace popl {
 class Diagnostics {
    public:
     static void Error(unsigned int line, std::string_view message);
     static void Error(Token token, std::string_view message);
+    static void ReportRunTimeError(const RunTimeError& error);
+    // mutually exclusive from run_time_error
     static bool HadError() { return ms_had_error; }
+    static bool HadRunTimeError() { return ms_had_runtime_error; }
     static void ResetError() { ms_had_error = false; }
 
    private:
@@ -18,5 +22,6 @@ class Diagnostics {
 
    private:
     static bool ms_had_error;
+    static bool ms_had_runtime_error;
 };
 }  // namespace popl
--- src/diagnostics.cpp
+++ src/diagnostics.cpp
@@ -4,9 +4,12 @@
 #include <print>
 #include <string_view>
 
+#include "popl/syntax/Exceptions/run_time_error.hpp"
+
 namespace popl {
 
-bool Diagnostics::ms_had_error = false;
+bool Diagnostics::ms_had_error         = false;
+bool Diagnostics::ms_had_runtime_error = false;
 
 void Diagnostics::Error(unsigned int line, std::string_view message) {
     Report(line, "", message);
@@ -18,6 +21,12 @@ void Diagnostics::Error(Token token, std::string_view message) {
     else
         Report(token.GetLine(), " at '" + token.GetLexeme() + "'", message);
 }
+
+void Diagnostics::ReportRunTimeError(const RunTimeError& error) {
+    std::println("{}\n[line {}]", error.what(), error.GetToken().GetLine());
+    ms_had_runtime_error = true;
+}
+
 void Diagnostics::Report(unsigned int line, std::string_view where,
                          std::string_view message) {
     ms_had_error = true;
