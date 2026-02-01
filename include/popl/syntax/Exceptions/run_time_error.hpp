73c723311a9bb04ee67def6c78e6374442039141
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-02-01 23:23:19 +0530

    Basic run time error class with token

---
+++ include/popl/syntax/Exceptions/run_time_error.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdexcept>
+
+#include "popl/lexer/token.hpp"
+
+namespace popl {
+class RunTimeError : public std::runtime_error {
+   public:
+    RunTimeError(Token token, std::string message)
+        : runtime_error{message}, m_token{token} {}
+    Token GetToken() const { return m_token; }
+
+   private:
+    Token m_token;
+};
+};  // namespace popl
