a41dcd0730567427a8b338d458787c228c6b927c
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-02-11 19:45:57 +0530

    Signal for continue and break statements

---
+++ include/popl/runtime/control_flow.hpp
@@ -0,0 +1,10 @@
+
+
+namespace popl::runtime::control_flow {
+/// Act as signals through exceptions.
+/// No information is needed. Internal only,
+/// not displayed to user.
+struct BreakSignal {};
+struct ContinueSignal {};
+
+};  // namespace popl::runtime::control_flow
