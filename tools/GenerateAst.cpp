8e2b3564d167329de4c19e88bf728c9d43533f84
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-02-11 19:45:37 +0530

    removed extra semicolon

--- tools/GenerateAst.cpp
+++ tools/GenerateAst.cpp
@@ -165,7 +165,7 @@ int main(int argc, char** argv) {
                     stmtBaseName, exprBaseName, stmtBaseName, stmtBaseName),
         std::format("While{}: {}* condition, {}* body", stmtBaseName,
                     exprBaseName, stmtBaseName),
-        std::format("Break{}: Token keyword;", stmtBaseName)};
+        std::format("Break{}: Token keyword", stmtBaseName)};
 
     DefineAst(exprBaseName, outputDir, ExprTypes);
     DefineAst(stmtBaseName, outputDir, StmtTypes);
