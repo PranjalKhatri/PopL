e139c91042067edf89d0c5419ffae52967aabbe5
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2026-02-20 18:47:31 +0530

    Added methods to class with getmethod

--- include/popl/runtime/popl_class.hpp
+++ include/popl/runtime/popl_class.hpp
@@ -2,22 +2,33 @@
 
 #include <memory>
 #include <string>
+#include <unordered_map>
 
 #include "popl/callables/callable.hpp"
+#include "popl/callables/popl_function.hpp"
 
 namespace popl {
 namespace runtime {
 class PoplClass : public popl::callable::PoplCallable,
                   public std::enable_shared_from_this<PoplClass> {
    public:
-    PoplClass(std::string name) : m_name(std::move(name)) {}
+    PoplClass(
+        std::string name,
+        std::unordered_map<std::string, std::shared_ptr<callable::PoplFunction>>
+            methods)
+        : m_name(std::move(name)), m_methods(std::move(methods)) {}
+
     popl::PopLObject Call(popl::Interpreter&                   interpreter,
                           const std::vector<popl::PopLObject>& args) override;
-    std::string      ToString() const override { return m_name; }
-    int              GetArity() const override { return 0; }
+    std::optional<std::shared_ptr<callable::PoplFunction>> GetMethod(
+        const std::string& name);
+    std::string ToString() const override { return m_name; }
+    int         GetArity() const override { return 0; }
 
    private:
     std::string m_name;
+    std::unordered_map<std::string, std::shared_ptr<callable::PoplFunction>>
+        m_methods;
 };
 }  // namespace runtime
 }  // namespace popl
--- src/popl_class.cpp
+++ src/popl_class.cpp
@@ -1,5 +1,7 @@
 #include "popl/runtime/popl_class.hpp"
 
+#include <optional>
+
 #include "popl/literal.hpp"
 #include "popl/runtime/popl_instance.hpp"
 
@@ -10,6 +12,13 @@ popl::PopLObject PoplClass::Call(popl::Interpreter& interpreter,
     auto instance = std::make_shared<PoplInstance>(shared_from_this());
 
     return popl::PopLObject{instance};
+}
+
+std::optional<std::shared_ptr<popl::callable::PoplFunction>>
+PoplClass::GetMethod(const std::string& name) {
+    auto it = m_methods.find(name);
+    if (it == m_methods.end()) return std::nullopt;
+    return it->second;
 }
 }  // namespace runtime
 }  // namespace popl
